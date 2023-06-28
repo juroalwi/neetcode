82bcc184607262c9f3b440089efab382219de61f
jralvarezwindey <jralvarezwindey@gmail.com>
2023-06-28 19:24:47 -0300

    Binary search.

---
+++ 4-binary-search/binary-search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l < r) {
+    let m = Math.floor((l + r) / 2);
+
+    if (nums[m] > target) {
+      r = m - 1;
+    } else if (nums[m] < target) {
+      l = m + 1;
+    }
+
+    if (nums[m] === target) {
+      return m;
+    }
+  }
+  return -1;
+};
