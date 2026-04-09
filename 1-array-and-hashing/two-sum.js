30db246e6fc9faa95ad71164b55b125d1f42a8a7
Julian Alvarez <juroalwi@gmail.com>
2026-04-08 23:11:41 -0300

    two sum O(n) solution

--- 1-array-and-hashing/two-sum.js
+++ 1-array-and-hashing/two-sum.js
@@ -1,7 +1,48 @@
-var twoSum = function (nums, target) {
+/**
+ * Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+ * 
+ * You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+ * 
+ * Return the answer with the smaller index first.
+ * 
+ * Example 1:
+ * Input: nums = [3,4,5,6], target = 7
+ * Output: [0,1]
+ * Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+ * 
+ * Example 2:
+ * Input: nums = [4,5,6], target = 10
+ * Output: [0,2]
+
+ * Example 3:
+ * Input: nums = [5,5], target = 10
+ * Output: [0,1]
+
+ * Constraints:
+ * 2 <= nums.length <= 1000
+ * -10,000,000 <= nums[i] <= 10,000,000
+ * -10,000,000 <= target <= 10,000,000
+ * Only one valid answer exists. 
+ */
+
+const twoSum = (nums, target) => {
+  const mem = new Map();
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j];
+    if (mem.has(nums[i])) {
+      return [mem.get(nums[i]), i];
+    }
+    const dif = target - nums[i];
+    if (!mem.has(dif)) {
+      mem.set(dif, i);
     }
   }
 };
+
+// Brute force:
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) return [i, j];
+//     }
+//   }
+// };
